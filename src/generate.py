a569ac40ba7787ec294c3820b661d1f53370e942
jara <jara@kb.dk>
2020-12-03 14:31:29 +0100

    language extension changed, and output in gitignore

--- src/generate.py
+++ src/generate.py
@@ -10,7 +10,7 @@ def html(obj, template_name, folder):
     if obj['language'] == 'dk':
         extension = f".html"
     else:
-        extension = f"{obj['language'].capitalize()}.html"
+        extension = f"-{obj['language']}.html"
     file_path = f"{folder}/{template_name}{extension}"
 
     html_file = open(file_path, 'w')
